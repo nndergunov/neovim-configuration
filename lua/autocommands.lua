d093f41b830b25263a6e17b7145b25ac5a90962f
ndergunov <ndergunov@gmail.com>
2022-06-26 14:29:09 +0300

    lsp autoformatting, full gofumpt support

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -11,7 +11,8 @@ vim.cmd([[
 vim.cmd([[
     augroup packer_user_config
         autocmd!
-        autocmd BufWritePre *.go :silent! lua require('go.format').gofmt()
+        autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()
+        autocmd BufWritePre *.go :silent! lua require('go.format').goimport()
         autocmd BufWritePre * call TrimWhitespace()
         autocmd BufWritePost * lua require('lint').try_lint()
         autocmd BufWritePost plugins.lua source <afile> | PackerCompile
--- lua/plugconfigs/lsp.lua
+++ lua/plugconfigs/lsp.lua
@@ -1,16 +1,8 @@
 local navic = require("nvim-navic")
 
-require'lspconfig'.gopls.setup {
-    settings = {
-        gopls = {
-            gofumpt = true
-        }
-    }
-}
-
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
@@ -19,28 +11,28 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  navic.attach(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    navic.attach(client, bufnr)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
 end
 
 -- tell the server the capability of foldingRange
@@ -51,16 +43,55 @@ capabilities.textDocument.foldingRange = {
     lineFoldingOnly = true
 }
 
--- Use a loop to conveniently call 'setup' on multiple servers and
--- map buffer local keybindings when the language server attaches
-local servers = {'gopls', 'clangd', 'dockerls', 'sumneko_lua', 'sqls', 'taplo', 'yamlls', 'rust_analyzer'}
-for _, lsp in pairs(servers) do
-  require('lspconfig')[lsp].setup {
+require 'lspconfig'.gopls.setup {
     capabilities = capabilities,
     other_fields = ...,
     on_attach = on_attach,
     flags = {
-      debounce_text_changes = 150,
+        debounce_text_changes = 150,
+    },
+    settings = {
+        gopls = {
+            gofumpt = true
+        }
     }
-  }
+}
+
+require 'lspconfig'.sumneko_lua.setup {
+    capabilities = capabilities,
+    other_fields = ...,
+    on_attach = on_attach,
+    flags = {
+        debounce_text_changes = 150,
+    },
+    settings = {
+        Lua = {
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+}
+
+-- Use a loop to conveniently call 'setup' on multiple servers and
+-- map buffer local keybindings when the language server attaches
+local servers = { 'clangd', 'dockerls', 'sqls', 'taplo', 'yamlls', 'rust_analyzer' }
+for _, lsp in pairs(servers) do
+    require('lspconfig')[lsp].setup {
+        capabilities = capabilities,
+        other_fields = ...,
+        on_attach = on_attach,
+        flags = {
+            debounce_text_changes = 150,
+        }
+    }
 end
